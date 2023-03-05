35bb047c3ca97ae9ca325bbc371c0a9e456bfa72
RamonJWS <raysantiago1994@gmail.com>
2023-03-05 14:24:34 +0000

    only one image for each blog post

--- main.py
+++ main.py
@@ -14,7 +14,6 @@ from db import models
 app = FastAPI()
 
 models.Base.metadata.create_all(engine)
-fake_db = []
 
 @app.post("/post")
 def create_blog_test(blog: BlogPost, db: Session = Depends(get_db)):
@@ -24,6 +23,15 @@ def create_blog_test(blog: BlogPost, db: Session = Depends(get_db)):
 @app.post("/post/image", response_class=FileResponse)
 def add_image(id: int, upload_file: UploadFile = File(...)):
     path = os.path.join("files", str(id) + "_" + upload_file.filename)
+
+    # check image exists for id, if so remove it.
+    files_start_with_id = [file for file in os.listdir("files") if file.startswith(str(id))]
+    if files_start_with_id:
+        for file in files_start_with_id:
+            os.remove(os.path.join('files', file))
+
+    # save image locally
+
     with open(path, "w+b") as file:
         shutil.copyfileobj(upload_file.file, file)
     return path
